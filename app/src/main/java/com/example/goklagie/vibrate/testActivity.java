a29526841f4638b8473cc32cde74bbb76f17d021
Enaduos <ena@duo.com>
2017-02-21 23:08:42 +0200

    adfas

--- app/src/main/java/com/example/goklagie/vibrate/testActivity.java
+++ app/src/main/java/com/example/goklagie/vibrate/testActivity.java
@@ -1,6 +1,7 @@
 package com.example.goklagie.vibrate;
 
-import android.content.Context;
+import android.content.res.Resources;
+import android.content.res.XmlResourceParser;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Vibrator;
@@ -9,82 +10,92 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
 
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
+import java.util.HashMap;
 
 public class testActivity extends AppCompatActivity {
+
+    private HashMap<String, String> elementList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
 
+        Resources res = getApplicationContext().getResources();
+        XmlResourceParser xpp = res.getXml(R.xml.vibrations);
+        elementList = new HashMap<String, String>();
+        String currentTag = new String();
+        String currentValue = new String();
+        String currentId = new String();
+        String currentAction = new String();
+        boolean newId = false;
+        boolean newAction = false;
 
-        InputStream ins = getResources().openRawResource(getResources().getIdentifier("vibration",
-                        "raw", getPackageName()));
-        //Context context = getApplicationContext();
-        //InputStream istream = context.getResources().openRawResource(R.raw.vibrate);
-//        //File inputFile = new File("app/src/main/res/xml/vibration.xml");
-        DocumentBuilderFactory dbFactory
-                = DocumentBuilderFactory.newInstance();
-        DocumentBuilder dBuilder = null;
         try {
-            dBuilder = dbFactory.newDocumentBuilder();
-        } catch (ParserConfigurationException e) {
-            e.printStackTrace();
-        }
-        Document doc = null;
-        try {
-            doc = dBuilder.parse(ins);
-        } catch (SAXException e) {
+            xpp.next();
+        } catch (XmlPullParserException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        //doc.getDocumentElement().normalize();
-        Log.d("asda","Root element :" + doc.getDocumentElement().getNodeName());
-        NodeList nList = doc.getElementsByTagName("element");
-        Log.d("asda","Root element :" + nList.getLength());
-        for (int temp = 0; temp < nList.getLength(); temp++) {
-            Node nNode = nList.item(temp);
-            System.out.println("\nCurrent Element :"
-                    + nNode.getNodeName());
-            Element eElement = (Element) nNode;
-            System.out.println("Student roll no : "
-                    + eElement.getElementsByTagName("action").item(0).getTextContent());
+        int eventType = 0;
+        try {
+            eventType = xpp.getEventType();
+        } catch (XmlPullParserException e) {
+            e.printStackTrace();
         }
+        while (eventType != XmlPullParser.END_DOCUMENT) {
+            if(eventType == XmlPullParser.START_DOCUMENT) {
+                Log.d(TAG,"Importing vibration preferences");
+            }
+            else if(eventType == XmlPullParser.START_TAG)  {
+                currentTag = xpp.getName();
+                if (currentTag.equals("id") && !newId) {
+                    newId = true;
+                }
+                if (currentTag.equals("action") && !newAction) {
+                    newAction = true;
+                }
+            }
+            else if(eventType == XmlPullParser.END_TAG) {
+                currentTag = xpp.getName();
+                if (currentTag.equals("element") && newId && newAction) {
+                    Log.d(TAG,"Id: " + currentId + " Action: " + currentAction);
+                    elementList.put(currentId, currentAction);
+                    newId = false;
+                    newAction = false;
+                }
+            }
+            else if(eventType == XmlPullParser.TEXT) {
+                currentValue = xpp.getText();
+                if (currentTag.equals("id") && newId) {
+                    currentId = currentValue;
+                }
+                if (currentTag.equals("action") && newAction) {
+                    currentAction = currentValue;
+                }
+            }
+            try {
+                eventType = xpp.next();
+            } catch (XmlPullParserException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }//eof-while
 
-        // Get instance of Vibrator from current Context
-        //Vibrator v = (Vibrator) getSystemService(this.VIBRATOR_SERVICE);
-
-        // Start without a delay
-        // Vibrate for 100 milliseconds
-        // Sleep for 1000 milliseconds
-        //long[] pattern = {0, 100, 1000};
-
-        // The '0' here means to repeat indefinitely
-        // '0' is actually the index at which the pattern keeps repeating from (the start)
-        // To repeat the pattern from any other point, you could increase the index, e.g. '1'
-        //v.vibrate(pattern, 0);
     }
     private static final String TAG = testActivity.class.getSimpleName();
 
-    private void getKids(ViewGroup group, Float x, Float y) {
+    private String getKids(ViewGroup group, Float x, Float y) {
         final int childcount = group.getChildCount();
         for (int i = 0; i < childcount; i++) {
             if (ViewGroup.class.isInstance(group.getChildAt(i))) {
-                getKids( (ViewGroup) group.getChildAt(i), x, y);
+                return getKids( (ViewGroup) group.getChildAt(i), x, y);
             } else {
                 View v = group.getChildAt(i);
                 int[] temp = new int[2];
@@ -95,12 +106,11 @@ public class testActivity extends AppCompatActivity {
                 Float height = (float) v.getHeight();
 
                 if (x>x1 & x<x1+width & y>y1 & y<y1+height){
-                    //return v.getId();
-                    Log.d(TAG, getResources().getResourceEntryName(v.getId()));
+                    return getResources().getResourceEntryName(v.getId());
                 }
-                //Log.d(TAG, getResources().getResourceEntryName(v.getId()));
             }
         }
+        return "1";
     }
 
     public boolean dispatchTouchEvent(MotionEvent event) {
@@ -108,45 +118,16 @@ public class testActivity extends AppCompatActivity {
         switch(eventaction) {
             case MotionEvent.ACTION_DOWN:
                 ViewGroup root = (ViewGroup) findViewById(android.R.id.content);
-                //final int childcount = root.getChildCount();
-                getKids(root, event.getX(), event.getY());
-                ViewGroup temp = (ViewGroup) root.getChildAt(0);
-                final int childcount = temp.getChildCount();
-
-                //for (int i = 0; i < childcount; i++) {
-                //    View v = temp.getChildAt(i);
-                //    Log.d(TAG,getResources().getResourceEntryName(v.getId()));
-                //}
-                //Log.d(TAG,Integer.toString(root.getId()));
-                //Log.d(TAG, "ssssssssssssssssssssssss:touch");
-                //Log.d(TAG,Float.toString(event.getX()));
+                String action = getKids(root, event.getX(), event.getY());
                 Vibrator v = (Vibrator) getSystemService(this.VIBRATOR_SERVICE);
-                long[] pattern = {0, 100, 1000};
-                //v.vibrate(pattern, 1);
-                v.vibrate((long) ((event.getX()+event.getY())/2));
+
+                Float temp = Float.valueOf(elementList.get(action));
+                float temp2 = (float) temp;
+                v.vibrate((long) temp2);
                 return true;
         }
 
         return super.onTouchEvent(event);
     }
-
-//    @Override
-//    public boolean onTouchEvent(MotionEvent event) {
-//
-//        int eventaction=event.getAction();
-//        switch(eventaction) {
-//            case MotionEvent.ACTION_DOWN:
-//                ViewGroup root = (ViewGroup) findViewById(android.R.id.content);
-//                Log.d(TAG,Integer.toString(root.getId()));
-//        }
-//        //Log.d(TAG, "ssssssssssssssssssssssss:touch");
-//        //Log.d(TAG,Float.toString(event.getX()));
-//        Vibrator v = (Vibrator) getSystemService(this.VIBRATOR_SERVICE);
-//        long[] pattern = {0, 100, 1000};
-//        //v.vibrate(pattern, 1);
-//        v.vibrate((long) ((event.getX()+event.getY())/2));
-//        return super.onTouchEvent(event);
-//    }
-
 
 }
